8c146f613b618a59887ae37d00a6eabfb0a08dcb
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-03-25 07:28:13 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -33,66 +33,15 @@ label>
 <label>
 <input type="file"
 id="image-file"
-accept="image\\\/*"
+accept="image/*"
 onChange={handleFileSelect}
  />
  {imageUrl && <img
  src={imageUrl} alt="Selected
  image" />}
  </label>
- <button
- onClick={handle Transcribe}
- Cha
- >Transcribe<\\\/button>
- {transcription && (
- <div>
- <h2>Transcription:<\\\/
- h2><p>{transcription}<\\\/p>
- <QRCode
- value={transcription} \import React, { useState } from
-'react';
-import Tesseract from
-'tesseract.js';
-import QRCode from
-'qrcode.react';
-function App() {
-const [imageUrl, setImageUrl] =
-useState("");
-const [transcription, 
-set Transcription] = useState("\");
-const handleFileSelect = e => {
-const file = e.target.files[0];
-const reader = new
-FileReader();
-reader.readAsData URL(file);
-reader.onload = () => {
-setImageUrl(reader.result);
-const handle Transcribe = async
-() => {
-const { data: {text }} = await
-Tesseract.recognize(imageUrl,
-setTranscription(text);
-'eng');
-};
-return (
-<div>
-<h1>
-Transcribe Image Text</ h1>
-<label htmlFor="image-file">Select an
-image file to transcribe:</
-label>
-<label
-<input type="file"
-id="image-file"
-accept="image/*"
-onChange={handleFileSelect}
- />
-{imageUrl && <img
-src={imageUrl} alt="Selected
-image" />}
 <button
 onClick={handleTranscribe}
-
 > Transcribe</button>
 {transcription && (
 <div>
@@ -101,8 +50,9 @@ h2><p>{transcription}</p>
 <QRCode
 value={transcription} />
  
- );
- }
+);
+
  </div>
 
- export default funtion  <App></App>;
+ export default funtion  App();
+</div>
